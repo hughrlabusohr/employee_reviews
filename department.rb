7ee0bcadc6f581cb377dae57d0c0f5b63caf2df6
Hugh Robert Labusohr The 2nd <hughlabusohr@mac.com>
2016-02-15 06:44:07 -0500

    turning in homework

--- department.rb
+++ department.rb
@@ -1,2 +1,10 @@
 class Department
+  attr_reader :name, :staff
+  def initialize (:name, :staff)
+    @name = name
+    @staff = []
+  end
+  def add_employee (new_employee)
+    staff = [] << new_employee
+  end
 end
