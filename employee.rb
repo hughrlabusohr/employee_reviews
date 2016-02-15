9ac480485b4a6716b6cefab66ef4da3de1f2315a
Hugh Robert Labusohr The 2nd <hughlabusohr@mac.com>
2016-02-15 06:44:38 -0500

    turning in homework

--- employee.rb
+++ employee.rb
@@ -1,3 +1,13 @@
 require './department.rb'
 class Employee
+  attr_reader :name, :email, :phone, :salary
+  def initialize(name, email, phone, salary)
+    @name = name
+    @email = email
+    @phone = phone
+    @salary = salary
+  end
+  # def eligible_for_raise.array = []
+  # def work_satisfactory?
+
 end
